4f05006edaf0e37559cbd45719e03d5d8f5097d5
YYH <ojbk@gmail.com>
2025-09-11 22:28:46 +0800

    init

---
+++ assets/user-CUxo6pE8.js
@@ -0,0 +1,1 @@
+import{D as m,q as r,E as t}from"./app-Bvap1A28.js";const l=m("user",()=>{const e=r(""),s=r(new Set),o=t(()=>Array.from(s.value)),u=t(()=>o.value.filter(a=>a!==e.value));function n(a){e.value&&s.value.add(e.value),e.value=a}return{setNewName:n,otherNames:u,savedName:e}});export{l as u};
